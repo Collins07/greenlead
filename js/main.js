7e1904a5301e9e860ae2f4e3987b1e59fce052cc
Collins07 <collinsabaya07@gmail.com>
2023-10-27 15:50:03 +0300

    AEC methodology

--- js/main.js
+++ js/main.js
@@ -14,3 +14,54 @@ sidebarBtn.onclick = function() {
 
 
 
+
+// Demo 1 of AEC methodology
+let count = 4000;
+const elementRead = document.querySelector('.read');
+const elementCarbon = document.querySelector('.carbon');
+const elementCredit = document.querySelector('.credit');
+
+function updateCounter1() {
+    if (count > 5000) {
+        count = 4000;
+    }
+
+    elementRead.textContent = count;
+    const calculatedValue = (count / 10) * 0.0023;
+    elementCarbon.textContent = calculatedValue.toFixed(3);
+    
+    const creditValue = calculatedValue * 5;
+    elementCredit.textContent = creditValue.toFixed(4);
+    
+    count++;
+}
+
+// Call the updateCounter1 function every 500 milliseconds
+setInterval(updateCounter1, 500);
+
+
+
+
+// Demo 2 of AEC methodology
+let count1 = 2000;
+const elementRead1 = document.querySelector('#read');
+const elementCarbon1 = document.querySelector('#carbon');
+const elementCredit1 = document.querySelector('#credit');
+
+function updateCounter2() {
+    if (count1 > 3500) {
+        count1 = 2000;
+    }
+
+    elementRead1.textContent = count1;
+    const calculatedValue1 = (count1 / 10) * 0.0023;
+    elementCarbon1.textContent = calculatedValue1.toFixed(3);
+    
+    const creditValue1 = calculatedValue1 * 5;
+    elementCredit1.textContent = creditValue1.toFixed(4);
+    
+    count1++;
+}
+
+// Call the updateCounter2 function every 500 milliseconds
+setInterval(updateCounter2, 100);
